3f321f12abfff9d49a87a037f449f1c62c299879
Arthur <76882612+KingLlch@users.noreply.github.com>
2024-04-12 17:32:36 +0300

    point color

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -321,17 +321,17 @@ public class GameManager : MonoBehaviour
 
         if (card.SelfCard.Points == card.SelfCard.MaxPoints)
         {
-            card.Point.color = Color.black;
+            card.Point.colorGradient = new VertexGradient(Color.white, Color.white, Color.black, Color.black);
         }
 
         else if (card.SelfCard.Points < card.SelfCard.MaxPoints)
         {
-            card.Point.color = Color.red;
+            card.Point.colorGradient = new VertexGradient(Color.red, Color.red, Color.black, Color.black);
         }
 
         else
         {
-            card.Point.color = Color.green;
+            card.Point.colorGradient = new VertexGradient(Color.green, Color.green, Color.black, Color.black);
         }
     }
 
